f90859a594ce3a609e8ed508cc2458bc3127dd97
hohfchns <hohfchns@gmail.com>
2021-05-07 18:32:57 +0300

    tried and failed to create hands rotating around player, will maybe try again later

---
+++ DeshanimQuest/Player/Components/FloatingHand.gd
@@ -0,0 +1,9 @@
+class_name FloatingHand
+extends Sprite
+
+enum Sides { LEFT, RIGHT }
+
+export(Sides) var side = Sides.RIGHT
+
+func _ready():
+	pass
--- DeshanimQuest/Player/Player.gd
+++ DeshanimQuest/Player/Player.gd
@@ -8,9 +8,24 @@ export var __max_speed = Vector2(200, 200)
 export var __acceleration_amt = 1500
 export var __friction_amt = 1200
 
+enum HandStates { BOTH, RIGHT, LEFT }
+var available_hands = HandStates.LEFT
+
+export var right_hand_distance = 5.0
+export var left_hand_distance = 5.0
+
+export(NodePath) onready var __right_hand = get_node(__right_hand) as FloatingHand
+export(NodePath) onready var __left_hand = get_node(__left_hand) as FloatingHand
+
+export(NodePath) onready var __right_hand_rot_point = get_node(__right_hand_rot_point) as Position2D
+export(NodePath) onready var __left_hand_rot_point = get_node(__left_hand_rot_point) as Position2D
+
 #export(NodePath) onready var __body_sprite = get_node(__body_sprite) as Sprite
 #export(NodePath) onready var __right_arm_sprite = get_node(__right_arm_sprite) as Sprite
 #export(NodePath) onready var __left_arm_sprite = get_node(__left_arm_sprite) as Sprite
+onready var __right_arm_sprite = $AnimSprites/RightArmSprite as Sprite
+onready var __left_arm_sprite = $AnimSprites/LeftArmSprite as Sprite
+
 
 export(NodePath) onready var __body_anim = get_node(__body_anim) as AnimationPlayer
 export(NodePath) onready var __right_arm_anim = get_node(__right_arm_anim) as AnimationPlayer
@@ -29,6 +44,9 @@ func _physics_process(delta):
 	__movement_code(delta)
 	
 	__animate_sprites()
+	__hide_taken_hands()
+	
+	__set_hands_distance()
 	
 	__last_input_vector = __input_vector if __input_vector else __last_input_vector 
 
@@ -47,22 +65,56 @@ func __movement_code(delta):
 	
 	move_and_collide(__velocity * delta)
 
+
+func __play_run_anim(side):
+	__body_anim.play("Run" + side)
+	__right_arm_anim.play("Run" + side)
+	__left_arm_anim.play("Run" + side)
+
+func __play_idle_anim(side):
+	__body_anim.play("Idle" + side)
+	__right_arm_anim.play("Idle" + side)
+	__left_arm_anim.play("Idle" + side)
+
 func __animate_sprites():
-	if __input_vector != Vector2.ZERO:
+	var moving: bool = __input_vector != Vector2.ZERO
+	if moving:
 		if __input_vector.x >= 0:
-			__body_anim.play("RunRight")
-			__right_arm_anim.play("RunRight")
-			__left_arm_anim.play("RunRight")
+			__play_run_anim("Right")
 		else:
-			__body_anim.play("RunLeft")
-			__right_arm_anim.play("RunLeft")
-			__left_arm_anim.play("RunLeft")
-	else:
+			__play_run_anim("Left")
+		
+		return
+	elif not moving:
 		if __last_input_vector.x >= 0:
-			__body_anim.play("IdleRight")
-			__right_arm_anim.play("IdleRight")
-			__left_arm_anim.play("IdleRight")
+			__play_idle_anim("Right")
 		else:
-			__body_anim.play("IdleLeft")
-			__right_arm_anim.play("IdleLeft")
-			__left_arm_anim.play("IdleLeft")
+			__play_idle_anim("Left")
+
+
+func __hide_taken_hands():
+	match available_hands:
+		HandStates.BOTH:
+			__right_arm_sprite.show()
+			__left_arm_sprite.show()
+			__right_hand.hide()
+			__left_hand.hide()
+			
+		HandStates.RIGHT:
+			__right_arm_sprite.show()
+			__left_arm_sprite.hide()
+			__right_hand.hide()
+			__left_hand.show()
+		HandStates.LEFT:
+			__right_arm_sprite.hide()
+			__left_arm_sprite.show()
+			__right_hand.show()
+			__left_hand.hide()
+
+func __set_hands_distance():
+	__right_hand_rot_point.look_at(get_viewport().get_mouse_position())
+	__left_hand_rot_point.look_at(get_viewport().get_mouse_position())
+	
+
+
+
