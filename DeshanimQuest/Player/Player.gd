e686e1900742f04f034ff7082a2ac66d2aeb244f
hohfchns <hohfchns@gmail.com>
2021-05-08 10:58:01 +0300

    Rethinking code and way of doing hands completely

--- DeshanimQuest/Player/Player.gd
+++ DeshanimQuest/Player/Player.gd
@@ -9,7 +9,7 @@ export var __acceleration_amt = 1500
 export var __friction_amt = 1200
 
 enum HandStates { BOTH, RIGHT, LEFT }
-var available_hands = HandStates.LEFT
+var taken_hands = HandStates.RIGHT
 
 export var right_hand_distance = 5.0
 export var left_hand_distance = 5.0
@@ -79,37 +79,45 @@ func __play_idle_anim(side):
 func __animate_sprites():
 	var moving: bool = __input_vector != Vector2.ZERO
 	if moving:
-		if __input_vector.x >= 0:
-			__play_run_anim("Right")
-		else:
-			__play_run_anim("Left")
-		
-		return
+		if taken_hands == HandStates.BOTH:
+			if __input_vector.x >= 0:
+				__play_run_anim("Right")
+			else:
+				__play_run_anim("Left")
+		elif taken_hands == HandStates.RIGHT:
+			if __right_hand_rot_point.rotation_degrees > -90 \
+			and __right_hand_rot_point.rotation_degrees < 90:
+				__play_run_anim("Right")
+			else:
+				__play_run_anim("Left")
+		elif taken_hands == HandStates.LEFT:
+			pass
 	elif not moving:
-		if __last_input_vector.x >= 0:
-			__play_idle_anim("Right")
-		else:
-			__play_idle_anim("Left")
+		pass
+#		if __last_input_vector.x >= 0:
+#			__play_idle_anim("Right")
+#		else:
+#			__play_idle_anim("Left")
 
 
 func __hide_taken_hands():
-	match available_hands:
+	match taken_hands:
 		HandStates.BOTH:
-			__right_arm_sprite.show()
-			__left_arm_sprite.show()
-			__right_hand.hide()
-			__left_hand.hide()
+			__right_arm_sprite.hide()
+			__left_arm_sprite.hide()
+			__right_hand.show()
+			__left_hand.show()
 			
 		HandStates.RIGHT:
-			__right_arm_sprite.show()
-			__left_arm_sprite.hide()
-			__right_hand.hide()
-			__left_hand.show()
-		HandStates.LEFT:
 			__right_arm_sprite.hide()
 			__left_arm_sprite.show()
 			__right_hand.show()
 			__left_hand.hide()
+		HandStates.LEFT:
+			__right_arm_sprite.show()
+			__left_arm_sprite.hide()
+			__right_hand.hide()
+			__left_hand.show()
 
 func __set_hands_distance():
 	__right_hand_rot_point.look_at(get_global_mouse_position())
