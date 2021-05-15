f5708dd1c4c5abd9a863a3eadd3235b516701732
hohfchns <hohfchns@gmail.com>
2021-05-15 12:17:57 +0300

    Reworked hand rotating around player
    Also added sword and animations for sword

--- DeshanimQuest/Player/Player.gd
+++ DeshanimQuest/Player/Player.gd
@@ -4,8 +4,10 @@ export var __max_speed = Vector2(200, 200)
 export var __acceleration_amt = 1500
 export var __friction_amt = 1200
 
-export(NodePath) var __right_hand_path; onready var __right_hand = get_node(__right_hand_path) as FloatingHand
-export(NodePath) onready var __right_hand_rot_point = get_node(__right_hand_rot_point) as Position2D
+#export(NodePath) var __right_hand_path; onready var __right_hand = get_node(__right_hand_path) as FloatingHand
+export(NodePath) onready var __hand_axis = get_node(__hand_axis) as Node2D
+export(NodePath) onready var __hand_rot_point = get_node(__hand_rot_point) as Node2D
+onready var __current_item = __hand_rot_point.get_child(0)
 
 export(NodePath) onready var __body_anim = get_node(__body_anim) as AnimationPlayer
 
@@ -16,7 +18,6 @@ var __input_vector = Vector2(0, 0)
 var __last_input_vector = Vector2(1, 0)
 
 var __velocity = Vector2(0, 0)
-
 
 func _ready():
 	pass
@@ -30,7 +31,9 @@ func _physics_process(delta):
 	
 	__animate_sprites()
 	
-	__set_hand_distance()
+	__set_hand_distance_and_rot()
+	
+	__get_player_input()
 	
 	__last_input_vector = __input_vector if __input_vector else __last_input_vector 
 
@@ -52,23 +55,43 @@ func __movement_code(delta):
 
 func __animate_sprites():
 	var moving: bool = __input_vector != Vector2.ZERO
-	var rot = __right_hand_rot_point.rotation_degrees
+	var rot = __hand_axis.rotation_degrees
 	
 	if moving:
-		if __right_hand.global_position > self.global_position:
+		if __current_item.global_position > self.global_position:
 			__body_anim.play("RunRight")
 		else:
 			__body_anim.play("RunLeft")
 	elif not moving:
-		if __right_hand.global_position > self.global_position:
+		if __current_item.global_position > self.global_position:
 			__body_anim.play("IdleRight")
 		else:
 			__body_anim.play("IdleLeft")
 
 
-func __set_hand_distance():
-	__right_hand_rot_point.look_at(get_global_mouse_position())
+func __set_hand_distance_and_rot():
 	
+	if get_global_mouse_position() > self.get_global_position():
+		__hand_axis.scale.x = 1
+	else:
+		__hand_axis.scale.x = -1
+	
+	__hand_rot_point.look_at(get_global_mouse_position())
+	
+
+
+func __play_current_item_anim(var anim_name: String):
+	var ap = __current_item.get_node("AnimationPlayer") as AnimationPlayer
+	ap.play(anim_name)
+
+
+func __get_player_input():
+	if Input.is_action_just_pressed("use_item"):
+		__play_current_item_anim("Main")
+	elif Input.is_action_just_pressed("alt_use_item"):
+		__play_current_item_anim("Alt")
+
+
 
 
 
