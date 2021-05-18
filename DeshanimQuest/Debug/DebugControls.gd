2a18ea298ab13810a8613e6d48518856652eb814
hohfchns <hohfchns@gmail.com>
2021-05-18 18:54:31 +0300

    Revert "Revert "Added fullscreen (enabled by default) and changed camera zoom (fov)""
    This reverts commit 8de9b3b3083ad8d691b4f6ac85f31a01c9b13673.

--- DeshanimQuest/Debug/DebugControls.gd
+++ DeshanimQuest/Debug/DebugControls.gd
@@ -4,3 +4,6 @@ extends Node
 func _input(event):
 	if event.is_action_pressed("debug_quit"):
 		get_tree().quit()
+		
+	elif event.is_action_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
