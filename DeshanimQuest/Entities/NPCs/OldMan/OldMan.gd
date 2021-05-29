d2735a2cbabb129952592235717d2c4c317f6388
hohfchns <hohfchns@gmail.com>
2021-05-29 11:45:17 +0300

    Added Dialogic and basic npc dialogue

--- DeshanimQuest/Entities/NPCs/OldMan/OldMan.gd
+++ DeshanimQuest/Entities/NPCs/OldMan/OldMan.gd
@@ -2,4 +2,15 @@ extends StaticBody2D
 
 
 func interact():
-	$Label.show()
+	if get_node_or_null("DialogNode"):
+		return
+	
+	var dialog = Dialogic.start("OldManHello")
+	dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+	dialog.connect("timeline_end", self, "_on_dialog_end")
+	$CanvasLayer.add_child(dialog)
+	get_tree().paused = true
+
+
+func _on_dialog_end(timeline_name):
+	get_tree().paused = false
