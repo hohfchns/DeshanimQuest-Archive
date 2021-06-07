599623c2f00a360848aca5a3b265651b378fba9e
hohfchns <hohfchns@gmail.com>
2021-06-07 16:21:02 +0300

    Fixed item drag drop bug and added pickups for Glock and Sword

--- DeshanimQuest/Items/Pickups/Pickup.gd
+++ DeshanimQuest/Items/Pickups/Pickup.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 export(NodePath) onready var __amount_text = get_node(__amount_text) as Label
 
+export(String) var item_name
 export(int) var amount = 1
 
 func _ready():
 	__amount_text.text = str(amount) if amount > 1 else "" 
 
 func interact():
-	PlayerInventory.inventory.add_item("Health Potion", amount)
+	PlayerInventory.inventory.add_item(item_name, amount)
 	queue_free()
