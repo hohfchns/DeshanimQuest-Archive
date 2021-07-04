6d915fd96c81c3b4319b9fbe5b253d6141c52562
hohfchns <hohfchns@gmail.com>
2021-07-04 23:10:35 +0300

    Added basic movement code for Tsnon

--- DeshanimQuest/Entities/Enemies/Tsnon/Tsnon.gd
+++ DeshanimQuest/Entities/Enemies/Tsnon/Tsnon.gd
@@ -1,20 +1,33 @@
 extends KinematicBody2D
 
-export var friction_amt: float = 20.0
+export var __friction_amt: float = 20.0
+export var __acceleration_amt: float = 300.0
+export var __max_speed: float = 75.0
 
-export(NodePath) onready var __jump_tween_h = get_node(__jump_tween_h) as Tween
-export(NodePath) onready var __jump_tween_v = get_node(__jump_tween_v) as Tween
+export(NodePath) onready var __sprite = get_node(__sprite) as Sprite
+export(NodePath) onready var __anim = get_node(__anim) as AnimationPlayer
 
-
-var __velocity = Vector2.ZERO
-
+var __velocity: Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	var target_position = GameManager.get_player(self.name).global_position
+	var direction = -(self.global_position.direction_to(target_position))
 	
-	__velocity = __velocity.move_toward(Vector2.ZERO, friction_amt * delta)
+	if target_position:
+		__velocity = __velocity.move_toward(direction * __max_speed, __acceleration_amt * delta)
+		
+		__anim.play("RunRight")
+	else:
+		__velocity = __velocity.move_toward(Vector2.ZERO, __friction_amt * delta)
+		
+		__anim.play("IdleRight")
+	
+	if direction.x > 0:
+		__sprite.flip_h = false
+	else:
+		__sprite.flip_h = true
 	
 	move_and_slide(__velocity)
-
