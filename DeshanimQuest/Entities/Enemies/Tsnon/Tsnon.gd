cedd2f13ae72dd16409bf28749d1cef524caceb2
hohfchns <hohfchns@gmail.com>
2021-07-17 22:03:33 +0300

    (Probably) finished Tsnon, and tweaked a few parameters to balance it

--- DeshanimQuest/Entities/Enemies/Tsnon/Tsnon.gd
+++ DeshanimQuest/Entities/Enemies/Tsnon/Tsnon.gd
@@ -3,9 +3,20 @@ extends KinematicBody2D
 onready var __ai_controller = $RangedEnemyAI
 onready var __anim = $AnimationPlayer
 onready var __shot_start = $ShotStartPos
+onready var __damage_number_indicator = $DamageNumberIndicator
+onready var __effects_animation_player = $EffectsAP
+onready var __stats = $Stats
+onready var __hurtbox = $Hurtbox
+
+export var __knockback_multiplier: float = 1.2
 
 
 var pellet = preload("res://Entities/Enemies/Tsnon/TsnonPellet.tscn")
+
+
+func _ready():
+	__stats.connect("no_health", self, "_on_stats_no_health")
+	__hurtbox.connect("area_entered", self, "_on_hurtbox_area_entered")
 
 
 func _process(delta):
@@ -33,3 +44,27 @@ func shoot():
 	
 	get_parent().add_child(pellet_inst)
 
+
+func __take_hit(damage: int, knockback_amt: float, hitter):
+	var knockback_to_take = knockback_amt \
+	* __knockback_multiplier \
+	* hitter.get_parent().global_position.direction_to(self.global_position)
+	
+	__ai_controller.actor_velocity += knockback_to_take
+	
+	__stats.subtract_health(damage)
+	
+	__damage_number_indicator.start(damage)
+	
+	__effects_animation_player.play("Flash")
+
+
+func on_hitray_hit(hitray: Hitray):
+	__take_hit(hitray.damage, hitray.knockback_amt, hitray)
+
+func _on_hurtbox_area_entered(area: Hitbox):
+	__take_hit(area.damage, area.knockback_amt, area)
+
+
+func _on_stats_no_health():
+	queue_free()
