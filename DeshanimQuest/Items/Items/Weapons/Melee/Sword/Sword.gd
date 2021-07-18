56d60528d9b9bacc37bdb28e5a94a695cd08c4af
hohfchns <hohfchns@gmail.com>
2021-07-18 17:04:33 +0300

    Made sword not hit through objects and fixed stab animation

--- DeshanimQuest/Items/Items/Weapons/Melee/Sword/Sword.gd
+++ DeshanimQuest/Items/Items/Weapons/Melee/Sword/Sword.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 export(NodePath) onready var anim = get_node(anim) as AnimationPlayer
 
+onready var wall_check = $WallCheckRay
+onready var swing_hitbox = $SwingHitbox
+onready var stab_hitbox = $Stabhitbox
+
+
 func use(action):
 	if anim.is_playing():
 		return
@@ -11,3 +16,11 @@ func use(action):
 	elif action == "alt":
 		anim.play("Stab")
 
+
+func check_for_walls():
+	if wall_check.is_colliding():
+		swing_hitbox.monitorable = false
+		stab_hitbox.monitorable = false
+	else:
+		swing_hitbox.monitorable = true
+		stab_hitbox.monitorable = true
