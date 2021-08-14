73140de390e1fea5a69b3101bdd0a34559ac0ce4
hohfchns <hohfchns@gmail.com>
2021-08-14 21:12:43 +0300

    Made better wall detection for sword/hitbox

--- DeshanimQuest/Components/Collisions/Hitbox/Hitbox.gd
+++ DeshanimQuest/Components/Collisions/Hitbox/Hitbox.gd
@@ -4,6 +4,31 @@ extends Area2D
 export(int) var damage
 export(int) var knockback_amt
 
+export(NodePath) var __wall_detect_area_path
+var __wall_detect_area: Area2D
+
+export(NodePath) var __actor_path
+var __actor
 
 func _ready():
-	pass
+	if __wall_detect_area_path:
+		__wall_detect_area = get_node(__wall_detect_area_path) as Area2D
+	
+	if __actor_path:
+		__actor = get_node(__actor_path)
+
+
+func check_for_walls():
+	if not __wall_detect_area.get_overlapping_areas():
+		print("return")
+		return
+	
+	var hurtbox_pos = __wall_detect_area.get_overlapping_areas()[0].global_position
+	
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(__actor.global_position, hurtbox_pos, [], 128)
+	
+	if result:
+		self.monitorable = false
+	else:
+		self.monitorable = true
